697352c01874eeaec42d145f6114a5bba28f82b9
achrefajmi11 <achrefajmi20@gmail.com>
2022-05-18 15:49:33 +0200

    fix css

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,7 +1,7 @@
 //import { DehazeTwoTone } from "@material-ui/icons";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./userList.css";
+import "./userList.css";    
 //import { DataGrid } from "@material-ui/data-grid";
 //import { DeleteOutline } from "@material-ui/icons";
 //import { Link } from "react-router-dom";
@@ -30,35 +30,35 @@ const response = await axios.get("http://localhost:3005/users")
     console.log("data=>" , data);
       
         return (
-          <div style={{marginTop: "1px" ,width: "5000px" }}>
-          
-           
-          <table className="styled-table">
-            
-            <thead>
-              <tr>
-                <th  style={{textAlign: "Center"}}>id</th>
-                <th  style={{textAlign: "Center"}}>fullName</th>
-                <th  style={{textAlign: "Center"}}>username</th>
-                <th  style={{textAlign: "Center"}}>matricule</th>
-          </tr>
-          </thead>
-          <tbody>
-            {data && data.map((item, index) =>{
+          <div className={"container-table"}> 
+          <div className={"sub-container"}>
+            <table className="styled-table">
+              
+              <thead>
+                <tr>
+                  <th  style={{textAlign: "Center"}}>id</th>
+                  <th  style={{textAlign: "Center"}}>fullName</th>
+                  <th  style={{textAlign: "Center"}}>username</th>
+                  <th  style={{textAlign: "Center"}}>matricule</th>
+            </tr>
+            </thead>
+            <tbody>
+              {data && data.map((item, index) =>{
 
-    return(
-      <tr key={index}>
-        <th scope="row" >{index +1}</th>
-    
-        <td>{item.fullName}</td>
-        <td>{item.username}</td>
+      return(
+        <tr key={index}>
+          <th scope="row" >{index +1}</th>
       
-        <td>{item.matricule}</td>
-    </tr>
-    );
-})}
-           </tbody>
-          </table>
+          <td>{item.fullName}</td>
+          <td>{item.username}</td>
+        
+          <td>{item.matricule}</td>
+      </tr>
+      );
+  })}
+            </tbody>
+            </table>
+          </div>
           </div>
         );
          };
